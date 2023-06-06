18abecf9293cfd82c0041c557dd3366a69d5e7e6
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-06-06 15:22:26 -0500

    Deploy
    Arreglo de archivos para Deplayment_01

--- PI-Videogames-main/client/src/App.js
+++ PI-Videogames-main/client/src/App.js
@@ -2,6 +2,9 @@ import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import Navbar from "./components/Navbar/Navbar";
 import { Home, Landing, Detail, Form } from "./views";
 import { Route } from "react-router-dom";
+import axios from 'axios';
+
+axios.defaults.baseURL= 'http://localhost:3001';
 
 function App() {
 
